0b93c6ed111380097bb5f3109608ef387d5ce4a7
Pat Patterson <pat@backblaze.com>
2022-10-13 11:29:07 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import ImagePage from "./pages/Todo/ImagePage";
+import ImagePage from "./pages/Images/ImagePage";
 import Login from "./pages/Login/Login";
 import Landing from "./pages/Landing/Landing";
 import { useGetUser } from "./hooks";
